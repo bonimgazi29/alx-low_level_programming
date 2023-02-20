590d00712dddb76cbaeff16f27d22990fb3bb564
bonisile <bonimgazi641@gmail.com>
2023-02-20 13:40:14 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - main function 
+ * main - main function
  *
  * Return: always 0
  */
-
 int main(void)
 {
 	int i;
@@ -16,5 +15,5 @@ int main(void)
 	for (j = 'a' ; j <= 'f' ; j++)
 		putchar(j);
 	putchar('\n');
-return (0);
+	return (0);
 }
