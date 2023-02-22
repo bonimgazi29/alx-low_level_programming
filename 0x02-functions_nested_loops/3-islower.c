e5351260bf9a62873ad492ebcbb274f7a0ee7273
bonisile <bonimgazi641@gmail.com>
2023-02-22 11:11:06 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,19 +2,17 @@
 
 /**
  * _islower - checks for lowercase character
- * @C: The character to be checked
+ * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
 
 {
-
 	if (c >= 97 && c <= 122)
 	{
-	_putchar('-');
-         return (1);
+
+	return (1);
 	}
-	_putchar(0);
 	return (0);
 }
