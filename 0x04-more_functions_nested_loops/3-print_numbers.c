92677083aaed72186f21f626af71c1f6932c1389
bonisile <bonimgazi641@gmail.com>
2023-03-06 13:38:16 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,9 +10,9 @@ void print_numbers(void)
 {
 	int x;
 
-	for (x = 0; <= 9; x++)
-	{
-		_putchar(x + '0');
-	}
-	_putchar('\n');
+for (x = 0; <= 9; x++)
+{
+_putchar(x + '0');
+}
+_putchar('\n');
 }
